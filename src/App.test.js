7a22a5a8581d42cd4c8e1566e2a19bc925dc90e7
DogeCnx <thiraphat.re0096@gmail.com>
2021-01-31 06:48:45 +0700

    Passed test renders without crashing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import timeState from "./Reducer/timeState";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  const store = createStore(
+    timeState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+
+  ReactDOM.render(<Provider store={store}><App /></Provider>, div)
+})
